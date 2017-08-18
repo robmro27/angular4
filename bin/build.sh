f28d19c172ffe2e101805505125d529fd20946e0
robmro27 <robert.mroz1985@gmail.com>
2017-08-18 17:39:53 +0200

    build with assets

--- bin/build.sh
+++ bin/build.sh
@@ -2,6 +2,7 @@
 
 cd client
 ng build --aot --prod --deploy-url="dist"
-cp dist/* ../web/dist
+cp -r dist/* ../web/dist
+cp -r src/assets/* ../web/assets
 cp ../web/dist/index.html ../app/Resources/views/default/index.html.twig
 
