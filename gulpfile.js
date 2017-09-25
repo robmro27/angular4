ad0a200bbea1b67e3b423113a4908bb1e836784a
robmro27 <robert.mroz1985@gmail.com>
2017-09-25 15:44:18 +0200

    gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,23 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 
+gulp.task('default', ['sass','minify-css']);
+
 gulp.task('sass', function () {
     return gulp.src('./web/css/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./web/css'));
 });
 
-gulp.task('sass:watch', function () {
-    gulp.watch('./web/css/sass/*.scss', ['sass']);
-});
 
-gulp.task('minify-css', function () {
+
+gulp.task('minify-css', ['sass'], function () {
     return gulp.src('./web/css/*.css')
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('./web/css/min'));
 });
+
+
+// gulp.task('sass:watch', function () {
+//     gulp.watch('./web/css/sass/*.scss', ['sass']);
+// });
